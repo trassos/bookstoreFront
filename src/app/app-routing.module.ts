74c5b5ddcd66fe9867b3538bf1deaf8807f43b7f
trassos <tvidalo@ymail.com>
2022-06-08 14:39:56 -0300

    Categoria em contato com o back

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CategoryReadComponent } from './components/views/category/category-read/category-read.component';
+import { HomeComponent } from './components/views/home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  }, 
+  {
+    path: 'categories',
+    component: CategoryReadComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,46 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+import { HeaderComponent } from "./components/template/header/header.component";
+
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { FooterComponent } from "./components/template/footer/footer.component";
+import { NavComponent } from "./components/template/nav/nav.component";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatListModule } from "@angular/material/list";
+import { MatIconModule } from "@angular/material/icon";
+import { HomeComponent } from "./components/views/home/home.component";
+import { MatCardModule } from "@angular/material/card";
+import {MatTableModule} from '@angular/material/table';
+
+import { CategoryReadComponent } from './components/views/category/category-read/category-read.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    HomeComponent,
+    CategoryReadComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatCardModule,
+    MatTableModule,
+    HttpClientModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
